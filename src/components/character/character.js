164fb2eb7717ec8ae9c22ed62d6aa860e6de6c00
KinkHoang <viethoangdn.it@gmail.com>
2022-08-09 15:10:10 +0700

    Remove sign un used messenger config

--- src/components/character/character.js
+++ src/components/character/character.js
@@ -25,7 +25,7 @@ export default function Dashboard (props){
         <div style={{ display: "flex", flexWrap:"wrap", paddingTop: "50px" }}>
 
             { props.filteredData && props.filteredData.map(item => 
-                <Wrapper key={ item.id } style={{ width: "40%" ,boxShadow: "0 0 8px rgba(0,0,0,0.16)", margin: "15px", padding: "15px" }} >
+                <Wrapper key={ item.id } style={{ width: "43%" ,boxShadow: "0 0 8px rgba(0,0,0,0.16)", margin: "15px", padding: "15px" }} >
                     <h4>Name: {item.name}</h4>
                     <h5>gender: {item.gender}</h5>
                     <h5>culture: {item.culture}</h5>
